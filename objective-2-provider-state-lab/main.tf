05a5511abb2145a69294ba43ab3148e0740d7ccc
Jose Navarro <312827078+JoseNavarro777@users.noreply.github.com>
2026-08-05 22:24:03 -0600

    Add Objective 2 provider and state lab

---
+++ objective-2-provider-state-lab/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "objective2" {
+  name     = "rg-terraform-objective2"
+  location = "East US"
+
+  tags = {
+    environment = "lab"
+    objective   = "2"
+  }
+}
+
